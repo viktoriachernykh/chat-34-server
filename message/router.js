2d73764bb202da4518e6e9b6fedfdba26dbe608a
viktoriachernykh <chernycv@gmail.com>
2020-02-14 11:58:23 +0100

    Send data over the stream

--- message/router.js
+++ message/router.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const Message = require("./model");
+const Sse = require("json-sse");
 
 const { Router } = express;
 
+const stream = new Sse();
+
 const router = Router();
+
+router.get("/stream", (request, response, next) => {
+  stream.updateInit("test");
+  stream.init(request, response);
+});
 
 router.get("/message", async function(request, response, next) {
   try {
@@ -16,10 +24,11 @@ router.get("/message", async function(request, response, next) {
 
 router.post("/message", async function(request, response, next) {
   try {
-    const { body } = request;
-    const { text } = body;
-    const entity = { text };
-    const message = await Message.create(entity);
+    // const { body } = request;
+    // const { text } = body;
+    // const entity = { text };
+    // const message = await Message.create(entity);
+    const message = await Message.create(request.body);
     response.send(message);
     // console.log(message.dataValues);
   } catch (error) {
